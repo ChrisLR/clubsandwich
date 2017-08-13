9036965ca6b791c74da6909f5d007330b6cc22d8
nethys <arzhul@gmail.com>
2017-08-13 16:20:05 -0400

    Started work on KeyAssignedListView

--- clubsandwich/ui/collection_list.py
+++ clubsandwich/ui/collection_list.py
@@ -162,3 +162,21 @@ class SettingsListView(FirstResponderContainerView):
             self.min_row = min(len(self.labels) - self.inner_height - 1, self.min_row + self.inner_height)
             self.set_first_responder_in_visible_area()
             return True
+
+
+class KeyAssignedListView(SettingsListView):
+    CHARACTER_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%?&*()_+^:'.|{}[]@"
+
+    def __init__(self, value_controls, value_column_width=16, *args, **kwargs):
+        symbols = (symbol for symbol in self.CHARACTER_SET)
+        label_control_pairs = []
+        for value_control in value_controls:
+            label_control_pairs.append((next(symbols, " "), value_control))
+
+        super().__init__(label_control_pairs, value_column_width=16, *args, **kwargs)
+
+    def terminal_read(self, val):
+        super().terminal_read(val)
+        char = chr(terminal.state(terminal.TK_WCHAR))
+        
+
